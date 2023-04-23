635b180895de93b96b7dff28a04715993425a8b3
azizdevfull <aziz16110904@gmail.com>
2023-04-23 18:34:03 +0500

    add auth translate

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,21 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'sms_failed' => 'Failed to send SMS code!',
+    'sms_sent'   => 'Send SMS Code To Your Phone Number!',
+    'sms_invalid' => 'Invalid verification code!',
+    'phone_verified' => 'Phone number verified',
+    'user_not_found' => 'User not found',
+    'already_verified' => 'You are already verified',
+    'invalid_input' => 'Invalid login details.',
+    'phone_not_verified' => 'Phone number not verified',
+    'login_success' => 'Login successfully!',
+    'phone_not_found' => 'Phone number address not found',
+    'invalid_code' => 'Invalid reset code',
+    'reset_success' => 'Password reset successfully!',
+    'logout' => 'User logged out successfully!',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'not_authenticated' => 'Unauthenticate'
 
 ];
---
+++ lang/uz/auth.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Kimlik tekshirish til qatlamalari
+    |--------------------------------------------------------------------------
+    |
+    | Quyidagi til qatlamalari kimlik tekshirish jarayonida foydalanuvchi
+    | uchun ko'rsatishimiz lozim bo'lgan xabarlar uchun ishlatiladi. Siz
+    | bu til qatlamalarini o'z maqsadingizga moslashtirishingiz mumkin.
+    |
+    */
+
+    'sms_failed' => 'SMS-kodni yuborish muvaffaqiyatsiz tugadi!',
+    'sms_sent'   => 'Telefon raqamingizga SMS-kod yuborildi!',
+    'sms_invalid' => 'Noto\'g\'ri tasdiq-kod!',
+    'phone_verified' => 'Telefon raqami tasdiqlandi',
+    'user_not_found' => 'Foydalanuvchi topilmadi',
+    'already_verified' => 'Sizning raqamingiz allaqachon tasdiqlangan',
+    'invalid_input' => 'Noto\'g\'ri kirish ma\'lumotlari.',
+    'phone_not_verified' => 'Telefon raqami tasdiqlanmagan',
+    'login_success' => 'Kirish muvaffaqiyatli amalga oshirildi!',
+    'phone_not_found' => 'Telefon raqami topilmadi',
+    'invalid_code' => 'Noto\'g\'ri qayta tiklash-kod',
+    'reset_success' => 'Parol muvaffaqiyatli tiklandi!',
+    'logout' => 'Foydalanuvchi muvaffaqiyatli chiqdi!',
+    'password' => 'Ta\'minlangan parol noto\'g\'ri.',
+    'throttle' => 'Kirishga urinishlar juda ko\'p. Iltimos, :seconds soniya ichida qayta urinib ko\'ring.',
+    'not_authenticated' => 'Ro\'xatdan o\'tilmagan',
+
+];
